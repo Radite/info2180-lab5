cedd24a725b542a7fb1de112e4d7699eafdf6c94
unknown <joshuameghoo@gmail.com>
2023-11-20 20:08:06 -0500

    Added Lookup Table

--- world.php
+++ world.php
@@ -6,21 +6,39 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-// Check if 'country' GET variable is set
-if (isset($_GET['country'])) {
+// Check if a country name is passed in the GET request
+if (isset($_GET['country']) && trim($_GET['country']) != '') {
     $country = $_GET['country'];
-    // Prepared statement to avoid SQL injection
-    $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
-    $stmt->execute(['country' => '%' . $country . '%']);
-} else {
-    // Default query to select all countries
-    $stmt = $conn->query("SELECT * FROM countries");
-}
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // Prepare a statement to prevent SQL injection
+    $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+    $stmt->bindValue(':country', '%' . $country . '%');
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} else {
+    // If no country is specified, select all countries
+    $stmt = $conn->query("SELECT * FROM countries");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= htmlspecialchars($row['name'], ENT_QUOTES, 'UTF-8') . ' is ruled by ' . htmlspecialchars($row['head_of_state'], ENT_QUOTES, 'UTF-8'); ?></li>
-<?php endforeach; ?>
-</ul>
+
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($results as $row): ?>
+        <tr>
+            <td><?= htmlspecialchars($row['name']) ?></td>
+            <td><?= htmlspecialchars($row['continent']) ?></td>
+            <td><?= htmlspecialchars($row['independence_year']) ?></td>
+            <td><?= htmlspecialchars($row['head_of_state']) ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
